b7fba6b3b109c2efe9d6170e180ae835f24b6329
Matthijs S. Berends <m.s.berends@umcg.nl>
2022-08-08 22:20:38 +0200

    doc update

--- README.md
+++ README.md
@@ -1,14 +1,45 @@
 # `hashing`
 
-This is a simple [Shiny app](https://shiny.rstudio.com) to encrypt laboratory data for sharing.
+This is a simple [Shiny app](https://shiny.rstudio.com) to encrypt data for safe, anonymised data transfer. It supports MS Excel files, SAS files, SPSS files, Stata files, and flat files (CSV, TSV, TXT).
 
-## Run this app locally
+## About the hashing
+
+The app uses the `openssl` package to provide [SHA-2 hashing](https://en.wikipedia.org/wiki/SHA-2) using [HMAC](https://en.wikipedia.org/wiki/HMAC) for adding a key or 'salt' (required for this app) and a bit size of 256 bits. According to the [`openssl` documentation](https://rdrr.io/cran/openssl/man/hash.html):
+
+> *In applications where hashes should be irreversible (such as names or passwords) it is often recommended to use a random key for HMAC hashing. This prevents attacks where we can lookup hashes of common and/or short strings. See examples. A common special case is adding a random salt to a large number of records to test for uniqueness within the dataset, while simultaneously rendering the results incomparable to other datasets.*
+
+For this reason, it is mandatory in this app to fill in a self-chosen key, that is private to your data-receiving partner. This way, the data-receiving partner can never reproduce the hashes generated with SHA-2, unless the partner knows the key. This renders the data anonymous.
+
+To hash in R without this Shiny app, do something like this:
+
+``` r
+library(openssl)
+example_data_set <- data.frame(labid = c("1837473", "2442728"),
+                               patid = c("987239487", "987239487"),
+                               date = c(Sys.Date() - 10, Sys.Date()))
+head(example_data_set)
+#>     labid     patid       date
+#> 1 1837473 987239487 2022-07-29
+#> 2 2442728 987239487 2022-08-08
+
+# The actual hashing, we use the key "TestKey123!"
+example_data_set$labid <- sha256(example_data_set$labid, key = "TestKey123!")
+example_data_set$patid <- sha256(example_data_set$patid, key = "TestKey123!")
+head(example_data_set)
+#>                               labid                              patid        date
+#> 1 cc4a284d642790577386544230cf94...  c7c94ce293db4f4598cb532084e3b2...  2022-07-29
+#> 2 50406f84c4013484d12353640fcc21...  c7c94ce293db4f4598cb532084e3b2...  2022-08-08
+```
+
+Please note that the `openssl::sha*()` functions require the input to be of class `character`. Numeric values have to be transformed using e.g. `as.character()`.
+
+## Run this app locally (requires R)
 
 1.  Download this file to your computer: [`app.R`](https://github.com/msberends/hashing/blob/main/app.R)
 
 2.  Load the app using either of these options:
 
-    1.  Open it in RStudio and press the "Run App", OR
+    1.  Open it in RStudio and press the "Run App" button, OR
 
     2.  Run `source("path/to/app.R")` using the command line
 
@@ -18,7 +49,33 @@ This is a simple [Shiny app](https://shiny.rstudio.com) to encrypt laboratory da
 
 ## Run this app in the cloud
 
-1.  Open the URL [https://berends.shinyapps.io/hashing/](http://berends.shinyapps.io/clinicalmicrobiology/)
+This app is published in the cloud at ShinyApps.io, the Shiny server of RStudio.
+
+1.  Open the URL [https://berends.shinyapps.io/hashing/](http://berends.shinyapps.io/hashing/)
 2.  Use the user interface (see below). **Uploaded files will not remain stored and will be deleted immediately after import.**
 
 ## User Interface
+
+The app requires to fill in a key first.
+
+![](./screen1.png)
+
+After filling in a key, several options become available to add data:
+
+![](./screen2.png)
+
+Immediately after uploading and processing the data, the uploaded file is removed from the app. This means that with closing the app you will lose your data.
+
+![](./screen3.png)
+
+Click the button to show the data:
+
+![](./screen4.png)
+
+Select the column names that must be hashed, and click the button again:
+
+![](./screen5.png)
+
+Download the results and share them!
+
+------------------------------------------------------------------------
--- app.R
+++ app.R
@@ -3,7 +3,7 @@
 # August 2022
 # Dr. Matthijs S. Berends
 # 
-# Simple Shiny app to encrypt laboratory data
+# Simple Shiny app to encrypt data for safe, anonymised data transfer
 # https://github.com/msberends/hashing
 
 required_pkg <- c("shiny", "openssl", "DT", "openxlsx")
@@ -20,27 +20,31 @@ library(openxlsx)
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Hashing Laboratory Data with SHA-256"),
+  titlePanel("Hashing Data with SHA-256 and HMAC"),
+  p(HTML("This is a simple Shiny app to encrypt data for safe, anonymised data transfer. The app uses the <code>openssl</code> package to provide <a href='https://en.wikipedia.org/wiki/SHA-2'>SHA-2 hashing</a> using <a href='https://en.wikipedia.org/wiki/HMAC'>HMAC</a> for adding a key or 'salt' (required for this app) and a bit size of 256 bits.")),
   
   sidebarLayout(
     sidebarPanel(
-      textInput("salt", "1. Fill in the 'salt', an institute-specific secret:", width = "100%"),
+      textInput("key", "1. Fill in a self-chosen key, e.g. an institute-specific secret:", width = "100%"),
+      p("Note: this key must be kept secret from the data-receiving partner!", style = "font-style: italic;"),
       hr(style = "border-top: 1px solid black;"),
       
       uiOutput("step2"),
       uiOutput("step3"),
       
-      p(strong("DATA WILL NOT BE STORED")),
-      p(strong("Open Source:")),
-      p(strong(a("https://github.com/msberends/hashing"))),
-      width = 4,
+      p(strong("* DATA WILL NOT BE STORED *")),
+      width = 5,
     ),
     
     mainPanel(
       uiOutput("mainpanel"),
-      width = 8,
+      width = 7,
     )
-  )
+  ),
+  
+  p(HTML("Read more here: <a href='https://github.com/msberends/hashing/blob/main/README.md'>https://github.com/msberends/hashing</a>")),
+  p(HTML("<strong>Author:</strong> Dr. Matthijs Berends<br><strong>Copyright:</strong> this is free and open-source software, licensed under GNU GPL-2.0<br><strong>Source:</strong> <a href='https://github.com/msberends/hashing/blob/main/app.R'>https://github.com/msberends/hashing/blob/main/app.R</a>")),
+  p("Supported file types: MS Excel, SAS, SPSS, Stata, flat files (CSV, TSV, TXT)"),
 )
 
 server <- function(input, output) {
@@ -48,29 +52,17 @@ server <- function(input, output) {
   data_encrypt <- NULL
   
   output$step2 <- renderUI({
-    if (trimws(input$salt) == "") {
+    if (trimws(input$key) == "") {
       tagList()
     } else {
       tagList(
         radioButtons("datatype", "2. Give the values that must be hashed, using:",
                      choices = c("An Excel file" = "xlsx",
                                  "A CSV/TSV/TXT file" = "csv",
-                                 "Values from clipboard" = "clip"),
-                     selected = NULL),
-        uiOutput("upload"),
-        hr(style = "border-top: 1px solid black;")
-      )
-    }
-  })
-  output$step2 <- renderUI({
-    if (trimws(input$salt) == "") {
-      tagList()
-    } else {
-      tagList(
-        radioButtons("datatype", "2. Give the values that must be hashed, using:",
-                     choices = c("An Excel file" = "xlsx",
-                                 "A CSV/TSV/TXT file" = "csv",
-                                 "Values from clipboard" = "clip"),
+                                 "An SAS file" = "sas",
+                                 "An SPSS file" = "spss",
+                                 "A Stata file" = "stata",
+                                 "Clipboard" = "clip"),
                      selected = NULL),
         uiOutput("upload"),
         hr(style = "border-top: 1px solid black;")
@@ -93,7 +85,26 @@ server <- function(input, output) {
   output$upload <- renderUI({
     if (input$datatype == "xlsx") {
       tagList(
-        fileInput("excel_file", "Select Excel file:", multiple = FALSE, accept = ".xlsx"),
+        p("This uses the 'openxlsx' R package.", style = "font-style: italic;"),
+        fileInput("excel_file", "Select Excel file:", multiple = FALSE, accept = c(".xlsx", ".xlsm")),
+        uiOutput("cols_to_hash")
+      )
+    } else if (input$datatype == "sas") {
+      tagList(
+        p("This uses the 'foreign' R package, which is part of base R.", style = "font-style: italic;"),
+        fileInput("sas_file", "Select SAS file:", multiple = FALSE),
+        uiOutput("cols_to_hash")
+      )
+    } else if (input$datatype == "spss") {
+      tagList(
+        p("This uses the 'foreign' R package, which is part of base R.", style = "font-style: italic;"),
+        fileInput("spss_file", "Select SPSS file:", multiple = FALSE, accept = ".sav"),
+        uiOutput("cols_to_hash")
+      )
+    } else if (input$datatype == "stata") {
+      tagList(
+        p("This uses the 'foreign' R package, which is part of base R.", style = "font-style: italic;"),
+        fileInput("stata_file", "Select Stata file:", multiple = FALSE, accept = ".dta"),
         uiOutput("cols_to_hash")
       )
     } else if (input$datatype == "csv") {
@@ -103,7 +114,7 @@ server <- function(input, output) {
                                 "Semicolon: ;" = ";",
                                 "Tab: \\t" = "\t"),
                     selected = ","),
-        fileInput("csv_file", "Select CSV/TSV/TXT file:", multiple = FALSE, accept = ".csv"),
+        fileInput("csv_file", "Select CSV/TSV/TXT file:", multiple = FALSE, accept = c(".csv", ".txt", "tsv")),
         uiOutput("cols_to_hash")
       )
     } else if (input$datatype == "clip") {
@@ -114,7 +125,7 @@ server <- function(input, output) {
                                 "semicolon: ;" = ";",
                                 "tab: \\t" = "\t"),
                     selected = ""),
-        textAreaInput("pasted_values", "Paste values here:", height = "250px"),
+        textAreaInput("pasted_values", "Paste the values here:", height = "250px"),
         uiOutput("cols_to_hash")
       )
     }
@@ -122,14 +133,37 @@ server <- function(input, output) {
   
   
   output$cols_to_hash <- renderUI({
+    # Excel
     if (input$datatype == "xlsx" && !is.null(input$excel_file)) {
       datafile <<- openxlsx::read.xlsx(input$excel_file$datapath)
       # remove the local file immediately
       unlink(input$excel_file$datapath)
+      
+      # SAS
+    } else if (input$datatype == "sas" && !is.null(input$sas_file)) {
+      datafile <<- foreign::read.ssd(input$sas_file$datapath)
+      # remove the local file immediately
+      unlink(input$sas_file$datapath)
+      
+      # SPSS
+    } else if (input$datatype == "spss" && !is.null(input$spss_file)) {
+      datafile <<- foreign::read.spss(input$spss_file$datapath)
+      # remove the local file immediately
+      unlink(input$spss_file$datapath)
+      
+      # Stata
+    } else if (input$datatype == "stata" && !is.null(input$stata_file)) {
+      datafile <<- foreign::read.dta(input$stata_file$datapath)
+      # remove the local file immediately
+      unlink(input$stata_file$datapath)
+      
+      # CSV / TSV / TXT
     } else if (input$datatype == "csv" && !is.null(input$csv_file)) {
-      datafile <<- readr::read_delim(input$csv_file$datapath, delim = input$delim)
+      datafile <<- read.delim(input$csv_file$datapath, sep = input$delim, stringsAsFactors = FALSE)
       # remove the local file immediately
       unlink(input$csv_file$datapath)
+      
+      # Clipboard
     } else if (!is.null(input$pasted_values) && trimws(input$pasted_values) != "") {
       if (input$pasted_delim == "") {
         datafile <<- data.frame(x = input$pasted_values, stringsAsFactors = FALSE)
@@ -138,7 +172,7 @@ server <- function(input, output) {
           actionButton("start_hash", "Click to Start Data Hashing")
         ))
       } else {
-        datafile <<- readr::read_delim(input$pasted_values, delim = input$pasted_delim)
+        datafile <<- read.delim(input$pasted_values, sep = input$pasted_delim)
       }
     } else {
       datafile <<- NULL
@@ -147,10 +181,10 @@ server <- function(input, output) {
       tagList()
     } else {
       tagList(
-        p("Uploaded data was deleted after import", style = "color: red;"),
+        p("Note: uploaded data file was already deleted after import. By closing this window, you will lose your data.", style = "color: red;"),
         checkboxGroupInput("cols", "Select columns to hash:",
                            choices = colnames(datafile),
-                           selected = 1),
+                           inline = TRUE),
         checkboxInput("keep_originals", "Keep original columns", value = FALSE),
         actionButton("start_hash", "Click to Start Data Hashing")
       )
@@ -169,7 +203,7 @@ server <- function(input, output) {
     cols <- isolate(input$cols)
     keep_cols <- isolate(input$keep_originals)
     for (col in cols) {
-      .new_hash_var <- as.character(openssl::sha256(df[, col, drop = TRUE], key = input$salt))
+      .new_hash_var <- as.character(openssl::sha256(df[, col, drop = TRUE], key = input$key))
       pos <- which(colnames(df) == col)
       cols_before <- seq_len(pos)
       if (pos + 1 <= ncol(df)) {
@@ -180,7 +214,9 @@ server <- function(input, output) {
       df <- data.frame(df[cols_before],
                        data.frame(.new_hash_var = .new_hash_var, stringsAsFactors = FALSE),
                        df[cols_after],
-                       stringsAsFactors = FALSE)
+                       stringsAsFactors = FALSE,
+                       check.names = FALSE,
+                       fix.empty.names = FALSE)
       colnames(df)[colnames(df) == ".new_hash_var"] <- paste0(col, "_hash")
     }
     if (keep_cols == FALSE) {
@@ -206,21 +242,21 @@ server <- function(input, output) {
   
   output$downloadbtn <- renderUI({
     tagList(
-      downloadButton("download_xlsx", "As Excel file"),
-      downloadButton("download_csv", "As CSV file"),
-      downloadButton("download_clip", "Copy to clipboard")
+      downloadButton("download_xlsx", "As Excel file", width = "100%"),
+      downloadButton("download_csv", "As CSV file", width = "100%"),
+      downloadButton("download_clip", "Copy to clipboard", width = "100%")
     )
   })
   
   output$mainpanel <- renderUI({
-    if (trimws(input$salt) == "") {
+    if (trimws(input$key) == "") {
       tagList() 
     } else {
       tagList(
-        h3("Table output"),
+        h3("Example Table Output"),
         DTOutput("tableOut"),
         hr(),
-        h3("Data Structure"),
+        h3("Example Data Structure"),
         verbatimTextOutput("verbatimOut")
       )
     }
